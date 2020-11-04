6953c72991ed9c4e515526c4ed4896e8b96693cb
Misha <mihasyaterminator@gmail.com>
2020-11-04 12:00:27 +0200

    Added creating tables in DatabaseController

--- miMessageServer/databasecontroller.cpp
+++ miMessageServer/databasecontroller.cpp
@@ -7,9 +7,13 @@ DatabaseController::DatabaseController(QObject *parent) : QObject(parent)
 
     if(!connection.open())
         qCritical("Cant open database");
-    QSqlQuery("CREATE TABLE \"groups_members\" (\"group_name\" TEXT, \"username\" TEXT);", connection).exec();
-    QSqlQuery("CREATE TABLE \"messages\" (\"message_id\" INTEGER, \"date_time\" TEXT, \"message_text\" TEXT,\"from_user\" TEXT, \"to_user\"	TEXT, \"group_name\" TEXT, PRIMARY KEY(\"message_id\"));", connection).exec();
-    QSqlQuery("CREATE TABLE \"users\" (\"id\" INTEGER, \"username\"	TEXT, \"hashed_password\" TEXT, PRIMARY KEY(\"id\"))", connection).exec();
+
+    QSqlQuery("CREATE TABLE 'Chat' ('id' INTEGER,'chat_name' TEXT,PRIMARY KEY('id'))", connection).exec(); // Chat
+    QSqlQuery("CREATE TABLE 'ChatMember' ('chat_id' INTEGER,'username' TEXT)", connection).exec(); // ChatMember
+    QSqlQuery("CREATE TABLE 'Group' ('id' INTEGER,'group_name' TEXT,PRIMARY KEY('id'))", connection).exec(); // Group
+    QSqlQuery("CREATE TABLE 'GroupMember' ('group_id' INTEGER,'username' TEXT)", connection).exec(); // GroupMember
+    QSqlQuery("CREATE TABLE 'Message' ('message_id' INTEGER,'date_time' TEXT,'message_text' TEXT,'from_user' TEXT,'to_user' TEXT,'group_name' TEXT,PRIMARY KEY('message_id'))", connection).exec(); // Message
+    QSqlQuery("CREATE TABLE 'User' ('username' TEXT,'hashed_password' TEXT)", connection).exec(); // User
 }
 
 void DatabaseController::addUser(QString username, QString hashedPassword)
